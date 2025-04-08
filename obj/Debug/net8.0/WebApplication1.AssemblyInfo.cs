1c8fe0d1d2a34c912620603b5166b4429ffbee50
solankiRitish <140726419+solankiRitish@users.noreply.github.com>
2025-04-08 08:29:24 +0530

    change in ui

--- obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b9b6c8675d2a1364e188d3723fffb9d78e5b4fa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e73393eb36249b98678d31fe277de732d2540ef2")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
